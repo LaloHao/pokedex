7e932150df0a9899fc6c109a2e206f96615f5b04
Eduardo Vazquez <lalohao@gmail.com>
2019-01-16 15:41:23 -0600

    Add module resolver

---
+++ assets/images/index.js
@@ -0,0 +1,1 @@
+export { default as LogoPokemon } from './logo-pokemon.png'
--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, Image } from 'react-native';
+import { LogoPokemon } from 'assets/Images';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -21,6 +14,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
+        <Image source={LogoPokemon} style={styles.logo} />
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
@@ -45,5 +39,9 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  logo: {
+    width: 189,
+    height: 70,
   },
 });
