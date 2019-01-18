9361e15ef53f2b6c643ecdd6316c0921198a9ca9
Eduardo Vazquez <lalohao@gmail.com>
2019-01-18 14:42:39 -0600

    Add card component

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@
 /* eslint-disable import/prefer-default-export */
 export { default as SearchBox } from './SearchBox';
 export { default as Header } from './Header';
+export { default as Card } from './Card';
---
+++ src/types/Card.js
@@ -0,0 +1,8 @@
+// @flow
+import type { Navigation } from './Navigation';
+
+export type Card = {
+  name: string,
+  uri: string,
+  navigation: Navigation,
+};
